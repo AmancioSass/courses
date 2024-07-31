332dea26df6871d0445d9b9f05491a640a8a5e9d
Amâncio Sassamba <maxagain707@gmail.com>
2024-07-31 21:14:27 +0100

    Create a list resulting of the multiplication instead of just print them

--- elixir/maths.exs
+++ elixir/maths.exs
@@ -3,12 +3,17 @@ defmodule Module.Math do
     
     def times(param1, param2), do: param1 * param2
     
-    def power(num), do: power(num, 1) 
+    def power(num), do: IO.inspect(mult(num, 1))
     
     def power(num, 13), do: IO.puts(".")
     def power(num, it) do
         IO.puts("#{num} x #{it} = #{num * it}")
         power(num, it + 1)
     end
+
+    def mult(num, 13), do: []
+    def mult(num, it) do 
+        [num * it] ++ mult(num, it + 1)
+    end  
 end
 
